613a459318b14d3d48201fdccb89dd83b28370e3
Vitaly <lub-olen@yandex.ru>
2022-07-28 21:27:25 +0300

    Initial Commit

---
+++ To Do List 2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To Do List 2022
+//
+//  Created by Vitally Ochnev on 28.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
